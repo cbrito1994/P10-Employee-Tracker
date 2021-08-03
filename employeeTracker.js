c9671992611e21c1eb0f8d50d21156ffafc4cd34
Carlos Brito <cbrito1994@gmail.com>
2021-08-02 22:06:12 -0500

    Created the funcionalities to add employees, roles and departments

--- employeeTracker.js
+++ employeeTracker.js
@@ -4,13 +4,10 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: 'localhost',
-  
     // Your port; if not 3306
     port: process.env.PORT,
-  
     // Your username
     user: process.env.USER,
-  
     // Be sure to update with your own MySQL password!
     password: process.env.PASSWORD,
     database: 'employeesDB',
@@ -69,85 +66,114 @@ const start = () => {
 }
 
 const allEmployees = () => {
-    console.log("all employees");
-    
+    connection.query("SELECT first_name, last_name FROM employee", (err, res) => {
+        console.log(res);
+    });
 }
 
 const allRoles = () => {
-    console.log("all roles")
+    connection.query("SELECT title FROM role", (err, res) => {
+        console.log(res);
+    });
 }
 
 const allDepartments = () => {
-    console.log("all departments");
+    connection.query("SELECT department_name FROM department", (err, res) => {
+        console.log(res);
+    });
 }
 
 const addEmployee = () => {
-    console.log("add employee");
+    const rolesArray = [];
+    connection.query('SELECT * FROM role', (err, res) => {
+        res.forEach((choices) => {
+            rolesArray.push(choices.title);
+        });
+        
+    })
     connection.query('SELECT * FROM employee', (err, res) => {
         if (err) throw err;
-        const rolesArray = [];
-        res.forEach(roles => {
-            rolesArray.push(roles.role_id)
-        });
-        console.log(rolesArray);
-    });
-
-    connection.query('SELECT * FROM employee WHERE ?', {
-        role_id: 'Manager'
-    },(err, res) => {
-        if (err) throw err;
-        const namesArray = [];
-        res.forEach(roles => {
-            namesArray.push(roles.first_name)
-        });
-        console.log(namesArray);
-    });
-    
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'firstName',
-            message: 'What is the employee first name?',
-        },{
-            type: 'input',
-            name: 'lastName',
-            message: 'What is the employee last name?',
-        },{
-            type: 'list',
-            name: 'role',
-            message: 'What is the employee role?',
-            choices: [
-                ''
-            ]
-        },{
-            type: 'list',
-            name: 'manager',
-            message: 'Who is the employee manager?',
-            choices: [
-                ''
-            ]
-        }
-    ]).then(answer => {
-        const query = 'INSERT INTO employee SET ?';
-        connection.query(query, {
-            first_name: answer.firstName,
-            last_name: answer.lastName,
-            role_id: answer.role,
-            manager_id: answer.manager
-        }, err => {
-            if (err) throw err;
-            console.log('The employee was created successfully!');
-            start();
+        
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the employee first name?',
+            },{
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the employee last name?',
+            },{
+                type: 'list',
+                name: 'role',
+                message: 'What is the employee role?',
+                choices() {
+                    rolesArray
+                },
+            },{
+                type: 'list',
+                name: 'manager',
+                message: 'Who is the employee manager?',
+                choices() {
+                    const choiceArray = [];
+                    res.forEach((choices) => {
+                        choiceArray.push(choices.manager_id);
+                    });
+                    return choiceArray;
+                },
+            }
+        ]).then(answer => {
+            const query = 'INSERT INTO employee SET ?';
+            connection.query(query, {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.role,
+                manager_id: answer.manager
+            }, err => {
+                if (err) throw err;
+                console.log('The employee was created successfully!');
+                start();
+            })
         })
-    })
+    });
 }
 
 const addDepartment = () => {
-    console.log("add department")
+    inquirer.prompt([{
+        name: 'NewDepartment',
+        type: 'list',
+        message: 'Please insert the type of department for the employees',
+        choices: ["Engineer", "Human Resources", "Finance and Legal", "Marketing and Sales",]
+    }]).then((answer) => {
+        console.log("You have successfully added the department!");
+    })
 }
 
 const addRole = () => {
-    console.log("add role")
+    console.log("add role")<
+    inquirer.prompt([{
+        name: 'title',
+        type: 'list',
+        message: 'Choose the title of the employee',
+        choices: [
+          "Talent Acquisition Manager",
+          "Talent Acquisition Assistant",
+          "Accountant",
+          "Lawyer",
+          "Manager",
+          "Executive",
+          "Jr Executive",
+          "Sr Developer",
+          "Frontend Developer",
+          "Backend Developer"
+        ]
+    },{
+        name: 'salary',
+        type: 'input',
+        message: 'What is the employee´s salary?'
+    }]).then((answers) => {
+        console.log("You have succesfully added the salary!");
+    })
 }
 
 const update = () => {
