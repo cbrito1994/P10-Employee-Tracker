6e37893ffe529b9e1caf2d95cab99dec5feb19bf
Carlos Brito <cbrito1994@gmail.com>
2021-08-02 22:04:02 -0500

    Created the database structure

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -5,7 +5,7 @@ USE employeesDB;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT NOT NULL,
-    name VARCHAR(30) NULL,
+    department_name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
 
@@ -25,3 +25,15 @@ CREATE TABLE employee (
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
+
+-- SELECT role.title
+-- FROM role
+-- INNER JOIN employee ON role.id=employee.role_id;
+
+-- SELECT employee.first_name, employee.last_name
+-- FROM employee
+-- INNER JOIN role ON employee.manager_id=1;
+
+-- SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+-- FROM Orders
+-- INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
