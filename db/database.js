14fcc3cf9e361900809a595346d38c0691e78a91
Carlos Brito <cbrito1994@gmail.com>
2021-08-05 13:52:27 -0500

    Corrected a typo

--- db/database.js
+++ db/database.js
@@ -5,43 +5,43 @@ class DataBase {
     this.connection = connection;
   }
 
-  findAllEmployees() {
+  searchAllEmployees() {
     return this.connection.query(
       "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     );
   }
 
-  findAllEmployeesByDepartment(departmentId) {
+  searchAllEmployeesByDepartment(departmentId) {
     return this.connection.query(
       "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
       departmentId
     );
   }
 
-  findAllEmployeesByManager(managerId) {
+  searchAllEmployeesByManager(managerId) {
     return this.connection.query(
       "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
       managerId
     );
   }
 
-  findAllDepartments() {
+  searchAllDepartments() {
     return this.connection.query(
       "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM department LEFT JOIN role ON role.department_id = department.id LEFT JOIN employee ON employee.role_id = role.id GROUP BY department.id, department.name"
     );
   }
 
-  findAllRoles() {
+  searchAllRoles() {
     return this.connection.query(
       "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
     );
   }
 
-  createEmployee(employee) {
+  addEmployee(employee) {
     return this.connection.query("INSERT INTO employee SET ?", employee);
   }
 
-  removeEmployee(employeeId) {
+  deleteEmployee(employeeId) {
     return this.connection.query(
       "DELETE FROM employee WHERE id = ?",
       employeeId
