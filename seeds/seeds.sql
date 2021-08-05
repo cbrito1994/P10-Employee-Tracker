468db430932fe8751d76d263926b7be34b44c668
Carlos Brito <cbrito1994@gmail.com>
2021-08-05 13:37:34 -0500

    Moved the seeds to its own folder

--- seeds/seeds.sql
+++ seeds/seeds.sql
@@ -3,30 +3,12 @@ USE employeesDB;
 
 /* Insert 3 Rows into your new table */
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Carlos", "Brito", 1, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sofia", "Rojas", 2, 2);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Enrique", "Rodriguez", 3, 1);
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Carolina", "Flores", 1, 2);
+VALUES ("Carlos", "Brito", 1, 1), ("Sofia", "Rojas", 2, 2), ("Enrique", "Rodriguez", 3, 1), ("Carolina", "Flores", 1, 2)
 
 -- ROLES
 INSERT INTO role (title, salary, department_id)
-VALUES ("Manager", 100.00, 1);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Broker", 200.10, 2);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("Administrative", 53.30, 1);
+VALUES ("Manager", 100.00, 1), ("Broker", 200.10, 2), ("Administrative", 53.30, 1);
 
 -- DEPARTMENTS
-INSERT INTO department (department_name)
-VALUES ("Marketing and Sales");
-
-INSERT INTO department (department_name)
-VALUES ("Human Resources");
+INSERT INTO department (name)
+VALUES ("Marketing, Sales, Finance, Accounting");
